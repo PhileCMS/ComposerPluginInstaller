dd37331d4dac227a2ccc1f667f19f801d753f655
Frank Nägler <fn@iosoft.de>
2014-01-12 21:32:27 +0100

    change namespace

--- src/Phile/Composer/PluginInstaller.php
+++ src/Phile/Composer/PluginInstaller.php
@@ -10,16 +10,16 @@ class PluginInstaller extends LibraryInstaller {
      * {@inheritDoc}
      */
     public function getPackageBasePath(PackageInterface $package) {
-        $prefix = substr($package->getPrettyName(), 0, 13);
-        if ('phile/plugin-' !== $prefix) {
+        $prefix = substr($package->getPrettyName(), 0, 17);
+        if ('phile-cms/plugin-' !== $prefix) {
             throw new \InvalidArgumentException(
                 'Unable to install plugin, phile plugin '
                 .'should always start their package name with '
-                .'"phile/plugin-"'
+                .'"phile-cms/plugin-"'
             );
         }
 
-        return 'plugins/'.substr($package->getPrettyName(), 13);
+        return 'plugins/'.substr($package->getPrettyName(), 17);
     }
 
     /**
