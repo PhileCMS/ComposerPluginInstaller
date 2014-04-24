70e0fe6d4dc3e762d7af3aeba96954593798384f
Frank Nägler <mail@naegler.net>
2014-04-24 10:51:42 +0200

    change installer to handle plugins for 1.0.0

--- src/Phile/Composer/PluginInstaller.php
+++ src/Phile/Composer/PluginInstaller.php
@@ -10,16 +10,7 @@ class PluginInstaller extends LibraryInstaller {
      * {@inheritDoc}
      */
     public function getPackageBasePath(PackageInterface $package) {
-        $prefix = substr($package->getPrettyName(), 0, 17);
-        if ('phile-cms/plugin-' !== $prefix) {
-            throw new \InvalidArgumentException(
-                'Unable to install plugin, phile plugin '
-                .'should always start their package name with '
-                .'"phile-cms/plugin-"'
-            );
-        }
-
-        return 'plugins/'.substr($package->getPrettyName(), 17);
+        return 'plugins/';
     }
 
     /**
