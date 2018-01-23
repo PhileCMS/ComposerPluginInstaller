bb5a04ba61720d0cc5672d09a570b272b145ed97
Schlaefer <openmail+sourcecode@siezi.com>
2018-01-23 22:50:15 +0100

    PSR compatible formating changes

--- src/Phile/Composer/PluginInstaller.php
+++ src/Phile/Composer/PluginInstaller.php
@@ -1,30 +1,33 @@
 <?php
-
 namespace Phile\Composer;
 
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
-class PluginInstaller extends LibraryInstaller {
+class PluginInstaller extends LibraryInstaller
+{
+
     /**
      * {@inheritDoc}
      */
-    public function getInstallPath(PackageInterface $package) {
-			$fullPath = ['plugins'];
-			$pathParts = explode('/', $package->getName());
-			foreach ($pathParts as $path) {
-				$path = str_replace('-', ' ', $path);
-				$path = ucwords(trim($path));
-				$path = lcfirst(str_replace(' ', '', $path));
-				$fullPath[] = $path;
-			}
-			return implode(DIRECTORY_SEPARATOR, $fullPath);
+    public function getInstallPath(PackageInterface $package)
+    {
+        $fullPath = ['plugins'];
+        $pathParts = explode('/', $package->getName());
+        foreach ($pathParts as $path) {
+            $path = str_replace('-', ' ', $path);
+            $path = ucwords(trim($path));
+            $path = lcfirst(str_replace(' ', '', $path));
+            $fullPath[] = $path;
+        }
+        return implode(DIRECTORY_SEPARATOR, $fullPath);
     }
 
     /**
      * {@inheritDoc}
      */
-    public function supports($packageType) {
+    public function supports($packageType)
+    {
         return 'phile-plugin' === $packageType;
     }
 }
