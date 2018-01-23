77ea358a7a8bf21ee3c831be9813a1e869b510b6
Schlaefer <openmail+sourcecode@siezi.com>
2018-01-23 22:52:28 +0100

    add code comments

--- src/Phile/Composer/PluginInstaller.php
+++ src/Phile/Composer/PluginInstaller.php
@@ -13,10 +13,14 @@ class PluginInstaller extends LibraryInstaller
     public function getInstallPath(PackageInterface $package)
     {
         $fullPath = ['plugins'];
+        // split vendor and package part
         $pathParts = explode('/', $package->getName());
         foreach ($pathParts as $path) {
+            // split name string into single words
             $path = str_replace('-', ' ', $path);
+            // uppercase the first character of each word 
             $path = ucwords(trim($path));
+            // join words and make the first character lowercase
             $path = lcfirst(str_replace(' ', '', $path));
             $fullPath[] = $path;
         }
